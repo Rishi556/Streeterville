fe41857ae2f4dcef7dcbe93b21b1d8342f2e3b78
Rishi Panthee <rrpanthe@ncsu.edu>
2022-09-08 21:18:15 -0400

    Move rest to its own router

--- routes/rest/index.go
+++ routes/rest/index.go
@@ -1,33 +1,21 @@
-package main
+package routes
 
 import (
-	restRoutes "EngineREST/routes/rest"
 	"net/http"
 
 	"github.com/go-chi/chi"
-	"github.com/go-chi/chi/middleware"
 )
 
-func main() {
-	//Read params
-	//args := os.Args[1:]
-
-	//const node = "https://engine.rishipanthee.com"
-
-	//if len(args) < 2 {
-	//	fmt.Println("Usage: ./engineREST <PORTNUMBER>")
-	//	os.Exit(1)
-	//}
-
+func RestRouter() http.Handler {
 	r := chi.NewRouter()
-	r.Use(middleware.Logger)
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("You have accessed the Hive Engine REST API. All data fetches that could be done using post requests to a JSONRPC server can be done here but using simple GET requests."))
 	})
-	r.Mount("/rest", restRoutes.RestRouter())
+	r.Mount("/blockchain", BlockchainRouter())
+	r.Mount("/nft", NFTRouter())
+	r.Mount("/stats", StatsRouter())
 	r.Get("/*", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("The page you attempted to access doesn't exist. We don't have proper documentation yet. We'll let you know when we do."))
 	})
-	http.ListenAndServe(":3000", r)
-
+	return r
 }
